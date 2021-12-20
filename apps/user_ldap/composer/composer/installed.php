9dfe0e117b4ed879791d2269038be85f973ac002
violethaze74 <65783327+violethaze74@users.noreply.github.com>
2021-12-20 00:01:43 -0500

    Initialize dependabot/npm_and_yarn/nextcloud/logger-2.1.0

---
+++ apps/user_ldap/composer/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'reference' => '62a814f4fbdec485e97e6b55a8320a02ced488bb',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'reference' => '62a814f4fbdec485e97e6b55a8320a02ced488bb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
